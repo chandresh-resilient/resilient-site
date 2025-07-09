eac61a85ecf60421a80f0994ffef1e2fbfcf725f
Siddharth Mishra <siddhuji0509@gmail.com>
2025-07-09 23:08:32 +0530

    force commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,27 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
-  /* config options here */
-  reactStrictMode: true,
-    trailingSlash: true,
+// const nextConfig: NextConfig = {
+//   // output: "export",
+//   // images: {
+//   //   unoptimized: true,
+//   // },
+//   /* config options here */
+//   reactStrictMode: true,
+//     trailingSlash: true,
 
-};
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // ✅ This replaces `next export`
+  images: {
+    unoptimized: true // ✅ Required for static export if using next/image
+  },
+  // If using basePath or trailingSlash, you can define here
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
